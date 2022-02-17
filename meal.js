22aeafbb06752b6a419f38f7ed13155f08d05855
Mohd Hanzala <95011688+hanzala2626@users.noreply.github.com>
2022-02-18 00:00:33 +0530

    Add files via upload

---
+++ meal.js
@@ -0,0 +1,9 @@
+const serachForm = document.querySelector('form');
+const serachResultDiv = document.querySelector('.search-result-item');
+const container= document.querySelector('.container');
+
+let searchQuery = '';
+
+serachForm.addEventListener('submit',(e)  =>{
+    e.preventDefault();
+})
