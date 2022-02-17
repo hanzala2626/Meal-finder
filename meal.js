251cbd746779c0047d18ae3012e3a8c69ff80f3f
Mohd Hanzala <95011688+hanzala2626@users.noreply.github.com>
2022-02-17 23:58:25 +0530

    Delete meal.js

--- meal.js
+++
@@ -1,9 +0,0 @@
-const serachForm = document.querySelector('form');
-const serachResultDiv = document.querySelector('.search-result-item');
-const container= document.querySelector('.container');
-
-let searchQuery = '';
-
-serachForm.addEventListener('submit',(e)  =>{
-    e.preventDefault();
-})
